79a0ad18339627a8a92cf7e56caad05c97909ff5
Aidil83 <aidilrozi83@gmail.com>
2021-07-07 03:29:02 -0500

    fetch data from frontend and created basic ui.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useQuery } from 'react-query';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const getPosts = async () => {
+		// I'm destructuring data and returning it so that I can avoid writing data.data
+		const { data } = await axios.get('http://localhost:3001/posts');
+		return data;
+	};
+	const { data } = useQuery('Posts', getPosts);
+
+	return (
+		<div className='App'>
+			{data?.map((value) => {
+				return (
+					<div className='post'>
+						<div className='title'> {value.title} </div>
+						<div className='body'>{value.postText}</div>
+						<div className='footer'>{value.username}</div>
+					</div>
+				);
+			})}
+		</div>
+	);
 }
 
 export default App;
